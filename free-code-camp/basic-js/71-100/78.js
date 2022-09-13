4d88ce3c81377af604eacc8b191984f617d16e73
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2022-09-13 20:22:41 +0300

    feat: pass 81 js tasks

---
+++ free-code-camp/basic-js/71-100/78.js
@@ -0,0 +1,19 @@
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  if ([2,3,4,5,6].includes(card)) {
+    count += 1
+  }
+  if ([7,8,9].includes(card)) {
+    count += 0
+  }
+  if ([10, 'J', 'Q', 'K', 'A'].includes(card)) {
+    count -= 1
+  }
+  return count > 0 ? `${count} Bet` : `${count} Hold`
+  
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
